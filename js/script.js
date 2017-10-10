f50d93e23e0600cf46ddcc24e6db5e53b819a479
Adrian J. De Niz <ajdeniz@gmail.com>
2017-10-10 11:52:39 -0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -9,11 +9,21 @@ width = canvas.width = window.innerWidth;
 height = canvas.height = window.innerHeight;
 canvas.style.backgroundPosition = cxCoordinate + "px";
 
+var xMin= Math.round(width * 0.5 / 100) * 10;
+var xMax = Math.round(width * 0.5 / 100) * 100;
+var steps = Math.round(width * 0.5 / 100);
+
+console.log(Math.round(width * 0.5 / 100) * 10);
+console.log(Math.round(width * 0.5 / 100) * 100);
+console.log(Math.round(width * 0.5 / 100));
+console.log(Math.round(height / 2));
+console.log(Math.round(width / 7));
+
 var rect1 = {
-      x: 100,
-      y: height - 570, // universal y position
-      width: 288,
-      height: 570,
+      x: xMin,
+      y: height - Math.round(height / 2), // universal y position
+      width: Math.round(width / 7),
+      height: Math.round(height / 2)
 };
 
 var imageObj = new Image();
@@ -54,14 +64,14 @@ function runImage() {
   }
 }
 
-
 (function loop() {
-  if(key[0] && rect1.x != 1000) {
-    cxCoordinate-=40;
+  if(key[0] && rect1.x != xMax) {
+    cxCoordinate -= Math.round(width/48);
     canvas.style.backgroundPosition = cxCoordinate + "px";
     context.clearRect(0, 0, width, height);
     runImage();
-    rect1.x+=10;
+    rect1.x += steps;
+    console.log(rect1.x);
     context.drawImage(imageObj, sxCoordinate, 0, 288, 557, rect1.x, rect1.y, rect1.width, rect1.height);
     if(utils.rectIntersect(rect0, rect1)) {
 			context.fillStyle = "#ff6666";
@@ -71,27 +81,11 @@ function runImage() {
 		}
     context.fillRect(rect0.x, rect0.y, rect0.width, rect0.height);
   }
-  if (key[0] && rect1.x == 1000){
-    cxCoordinate-=40;
+  if (key[0] && rect1.x == xMax){
+    cxCoordinate -= Math.round(width/48);
     canvas.style.backgroundPosition = cxCoordinate + "px";
     context.clearRect(0, 0, width, height);
     runImage();
-    context.drawImage(imageObj, sxCoordinate, 0, 288, 557, rect1.x, rect1.y, rect1.width, rect1.height);
-    if(utils.rectIntersect(rect0, rect1)) {
-			context.fillStyle = "#ff6666";
-		}
-		else {
-			context.fillStyle = "#999999";
-		}
-    context.fillRect(rect0.x, rect0.y, rect0.width, rect0.height);
-  }
-
-  if(key[1] && rect1.x != 100) {
-    cxCoordinate+=40;
-    canvas.style.backgroundPosition = cxCoordinate + "px";
-    context.clearRect(0, 0, width, height);
-    runImage();
-    rect1.x-=10;
     context.drawImage(imageObj, sxCoordinate, 0, 288, 557, rect1.x, rect1.y, rect1.width, rect1.height);
     if(utils.rectIntersect(rect0, rect1)) {
 			context.fillStyle = "#ff6666";
@@ -102,8 +96,25 @@ function runImage() {
     context.fillRect(rect0.x, rect0.y, rect0.width, rect0.height);
   }
 
-  if(key[1] && rect1.x == 100) {
-    cxCoordinate+=40;
+  if(key[1] && rect1.x != xMin) {
+    cxCoordinate += Math.round(width/48);
+    canvas.style.backgroundPosition = cxCoordinate + "px";
+    context.clearRect(0, 0, width, height);
+    runImage();
+    rect1.x -= steps;
+    console.log(rect1.x);
+    context.drawImage(imageObj, sxCoordinate, 0, 288, 557, rect1.x, rect1.y, rect1.width, rect1.height);
+    if(utils.rectIntersect(rect0, rect1)) {
+			context.fillStyle = "#ff6666";
+		}
+		else {
+			context.fillStyle = "#999999";
+		}
+    context.fillRect(rect0.x, rect0.y, rect0.width, rect0.height);
+  }
+
+  if(key[1] && rect1.x == xMin) {
+    cxCoordinate += Math.round(width/48);
     canvas.style.backgroundPosition = cxCoordinate + "px";
     context.clearRect(0, 0, width, height);
     runImage();
@@ -133,7 +144,7 @@ function runImage() {
     context.fillRect(rect0.x, rect0.y, rect0.width, rect0.height);
     jump = setTimeout(function () {
       context.clearRect(0, 0, width, height);
-      rect1.y = height - 570;
+      rect1.y = height - Math.round(height / 2);
       context.drawImage(imageObj, sxCoordinate, 0, 288, 557, rect1.x, rect1.y, rect1.width, rect1.height);
       if(utils.rectIntersect(rect0, rect1)) {
   			context.fillStyle = "#ff6666";
