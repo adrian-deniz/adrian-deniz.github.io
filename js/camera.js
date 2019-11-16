1d8747a1f6ec9de6811eff13c6aa86a17da321ba
Adrian J. De Niz <ajdeniz@gmail.com>
2019-11-16 11:39:59 -0800

    Update camera.js

--- js/camera.js
+++ js/camera.js
@@ -1,10 +1,11 @@
-function startup() {
-    navigator.mediaDevices.getUserMedia({
-        audio: false,
-        video: true
-    }).then(stream => {
-        video.srcObject = stream;
-    }).catch(console.error)
-}
+var constraints = { audio: false, video: { width: 1280, height: 720 } }; 
 
-window.addEventListener('load', startup, false);
+navigator.mediaDevices.getUserMedia(constraints)
+.then(function(mediaStream) {
+  var video = document.querySelector('video');
+  video.srcObject = mediaStream;
+  video.onloadedmetadata = function(e) {
+    video.play();
+  };
+})
+.catch(function(err) { console.log(err.name + ": " + err.message); }); // always check for errors at the end.
